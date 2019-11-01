81485bd400382f611356bc26612e872eed233b8f
DanielKRing1 <DanielKRing1@gmail.com>
2019-11-01 13:38:32 -0700

    Init test server

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    const rng = Math.random();
+
+    const payload = JSON.stringify({
+        processID: process.pid,
+        rng
+    });
+
+    console.log(`Rng ${rng}...\n...from Process ${process.pid}\n\n`)
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(payload);
+}).listen(3000);
